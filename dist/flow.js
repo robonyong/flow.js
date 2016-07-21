d33f6d68a77844b7c4106354f15a985483fb46d2
Robin Yang <robinyone@gmail.com>
2016-07-21 13:20:40 -0700

    Release v2.11.4

--- dist/flow.js
+++ dist/flow.js
@@ -1605,7 +1605,7 @@
    * Library version
    * @type {string}
    */
-  Flow.version = '2.11.2';
+  Flow.version = '2.11.4';
 
   if ( typeof module === "object" && module && typeof module.exports === "object" ) {
     // Expose Flow as module.exports in loaders that implement the Node
